896b921d1af2092f97eb0acd0ebd67e57da4c97f
KseniyaGulina <75985144+KseniyaGulina@users.noreply.github.com>
2023-04-13 20:13:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from hyppo.ksample import MMD
-
+from scipy import stats
+from scipy.stats import anderson_ksamp
 
 chat_id = 1066531890 # Ваш chat ID, не меняйте название переменной
 
@@ -10,6 +11,8 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.05
-    p_value = MMD(compute_kernel = "rbf", gamma = 0.5).test(x, y)[1]
+    #p_value = MMD(compute_kernel = "rbf", gamma = 0.5).test(x, y)[1]
+    #p_value = stats.cramervonmises_2samp(x, y).pvalue
+    p_value = anderson_ksamp([x, y]).pvalue 
     result = p_value < alpha
     return result
